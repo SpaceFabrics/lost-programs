5da821d4cad49f7a956a02da99e318ac5afa9d83
SpaceFabrics <76684574+SpaceFabrics@users.noreply.github.com>
2023-12-12 14:53:21 -0500

    Update secretsanta.py

--- secretsanta.py
+++ secretsanta.py
@@ -2,7 +2,7 @@ import random
 from tkinter import *
 from tkinter import ttk
 
-version = 0.51
+version = 0.53
 
 def main(number):
     print("Secret Santa " + str(number))
@@ -35,7 +35,7 @@ def headings():
 
 def list_creation():
     global names
-    names = ['hi']
+    names = []
 
 list_creation()
 
@@ -54,29 +54,29 @@ def participants():
     print(names)
 
 def name_positions():
-    first_name1 = Label(window, text= names[1])
+    first_name1 = Label(window, text= names[0])
     first_name1.place(x=55,y=65)
-    first_name2 = Label(window, text= names[2])
+    first_name2 = Label(window, text= names[1])
     first_name2.place(x=55, y=95)
-    first_name3 = Label(window, text= names[3])
+    first_name3 = Label(window, text= names[2])
     first_name3.place(x=55, y=125)
-    first_name4 = Label(window,text= names[4])
+    first_name4 = Label(window,text= names[3])
     first_name4.place(x=55, y=155)
-    second_name1 = Label(window, text= names[5])
+    second_name1 = Label(window, text= names[4])
     second_name1.place(x=190, y=65 )
-    second_name2 = Label(window, text= names[6])
+    second_name2 = Label(window, text= names[5])
     second_name2.place(x=190, y=95)
-    second_name3 = Label(window, text= names[7])
+    second_name3 = Label(window, text= names[6])
     second_name3.place(x=190, y=125)
-    second_name4 = Label(window, text= names[8])
+    second_name4 = Label(window, text= names[7])
     second_name4.place(x=190, y= 155)
-    third_name1 = Label(window, text= names[9])
+    third_name1 = Label(window, text= names[8])
     third_name1.place(x=310, y=65)
-    third_name2 = Label(window, text= names[10])
+    third_name2 = Label(window, text= names[9])
     third_name2.place(x=310, y=95)
-    third_name3 = Label(window, text= names[11])
+    third_name3 = Label(window, text= names[10])
     third_name3.place(x=310, y=125)
-    third_name4 = Label(window, text= names[12])
+    third_name4 = Label(window, text= names[11])
     third_name4.place(x=310, y=155)
 
 def fam_setters():
@@ -96,29 +96,33 @@ def givers():
     num11 = random.randint(0,11)
     num12 = random.randint(0,11)
     
-    while (num1 == num2 or num1 == num3 or num1 == num4 or num1 == num5 or num1 == num6 
+    while(num1 == num2 or num1 == num3 or num1 == num4 or num1 == num5 or num1 == num6 
            or num1 == num7 or num1 == num8 or num1 == num8 or num1 == num9 or num1 == num10
            or num1 == num11 or num1 == num12 or num2 == num3 or num2 == num4 or num2 == num5 
            or num2 == num6 or num2 == num7 or num2 == num8 or num2 == num9 or num2 == num10 
            or num2 == num11 or num2 == num12 or num3 == num4 or num3 == num5 or num3 == num6 
            or num3 == num7 or num3 == num8 or num3 == num9 or num3 == num10 or num3 == num11 
            or num3 == num12 or num4 == num5 or num4 == num6 or num4 == num7 or num4 == num8 
-           or num4 == num9 
+           or num4 == num9 or num4 == num10 or num4 == num11 or num4 == num12 or num5 == num6 
+           or num5 == num7 or num5 == num8 or num5 == num9 or num5 == num10 or num5 == num11 or num5 == num12
+           or num6 == num7 or num6 == num8 or num6 == num9 or num6 == num10 or num6 == num11 or num12 
+           or num7 == num8 or num7 == num9 or num7 == num10 or num7 == num11 or num7 == num12
+           or num8 == num9 or num8 == num10 or num8 == num11 or num8 == num12 or num9 == num10 
+           or num9 == num11 or num9 == num12 or num10 == num11 or num10 == num12 or num11 == num12):
+        num1 = random.randint(0,11)
+        num2 = random.randint(0,11)
+        num3 = random.randint(0,11)
+        num4 = random.randint(0,11)
+        num5 = random.randint(0,11)
+        num6 = random.randint(0,11)
+        num7 = random.randint(0,11)
+        num8 = random.randint(0,11)
+        num9 = random.randint(0,11)
+        num10 = random.randint(0,11)
+        num11 = random.randint(0,11)
+        num12 = random.randint(0,11)
 
-           or num5 == num6 or num5 == num7 or num5 == num8
-           or num5 == num9 or num6 == num7 or num6 == num8 or num6 == num9 or num7 == num8
-           or num7 == num9 or num8 == num9):
-        num1 = random.randint(0,9)
-        num2 = random.randint(0,9)
-        num3 = random.randint(0,9)
-        num4 = random.randint(0,9)
-        num5 = random.randint(0,9)
-        num6 = random.randint(0,9)
-        num7 = random.randint(0,9)
-        num8 = random.randint(0,9)
-        num9 = random.randint(0,9)
-
-    print(names[num1], names[num2], names[num3], names[num4], names[num5], names[num6], names[num7], names[num8], names[num9])
+    print(names[num1], names[num2], names[num3], names[num4], names[num5], names[num6], names[num7], names[num8], names[num9], names[num10], names[num11], names[num12])
     giver1 = Label(window, text=names[num1])
     giver1.place(x=615, y=60)
     giver2 = Label(window, text=names[num2])
@@ -137,6 +141,12 @@ def givers():
     giver8.place(x=615, y=270)
     giver9 = Label(window, text=names[num9])
     giver9.place(x=615, y=300)
+    giver10 = Label(window, text=names[num10])
+    giver10.place(x=615,y= 330)
+    giver11 = Label(window, text=names[num11])
+    giver11.place(x=615,y=360)
+    giver12 = Label(window, text=names[num12])
+    giver12.place(x=615,y=390)
 
 
 
