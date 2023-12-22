20ef12426cd3f59c667978e8d9164637166f3b43
SpaceFabrics <76684574+SpaceFabrics@users.noreply.github.com>
2023-12-21 20:44:35 -0500

    update

--- PizzaStoreTracker/StoreData.py
+++ PizzaStoreTracker/StoreData.py
@@ -1,15 +1,35 @@
 from tkinter import *
 from tkinter import ttk
+import sys
+import cv2
+import pizza.jpg
 
 version = 0.01
+
+img = cv2.imread("pizza.jpg", cv2.IMREAD_ANYCOLOR)
 
 def main(number):
     print("Pizza Data Tracker " + str(number))
 
-def menu():
+def Window():
     global window
     window = Tk()    
     window.geometry('1000x500')
     window.title("Data Tracker " + str(version))
 
+    cv2.imshow("Pizza", img)
+    cv2.waitKey(0)
+
+    Start()
+
+
+
+    window.mainloop()
+
+def Start():
+    L1 = Label(window, text="Main Menu")
+    L1.place(x=35, y=35)
+
+
 main(version)
+Window()
